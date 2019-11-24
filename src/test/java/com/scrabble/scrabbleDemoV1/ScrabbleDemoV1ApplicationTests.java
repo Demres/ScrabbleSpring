4e7fbdab5351167b1b3202abe22938b138deaf72
Demres <bialy1423@gmail.com>
2019-11-24 18:21:19 +0100

    First try

---
+++ src/test/java/com/scrabble/scrabbleDemoV1/ScrabbleDemoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrabble.scrabbleDemoV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleDemoV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
